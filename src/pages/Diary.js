f990d441ff1907ff5e1957e31da9150cd5c1442c
hwanghaein <hhi9037@naver.com>
2023-09-20 16:03:31 +0900

    Router 응용

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,5 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <div>
+ 
+ const {id} = useParams();
+ console.log(id);
+ 
+ 
+ return <div>
     <h1>Diary</h1>
     <p>이곳은 일기 상세 페이지입니다.</p>
   </div>
