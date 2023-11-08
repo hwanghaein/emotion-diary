b82a37bda37e348f8f17f203e3116ea340ea0025
hwanghaein <hhi9037@naver.com>
2023-11-08 21:43:27 +0900

    헤더 및 날짜 구현

--- src/App.js
+++ src/App.js
@@ -40,11 +40,44 @@ return newState;  // (8)
 export const DiaryStateContext = React.createContext(); // (21)
 export const DiaryDispatchContext = React.createContext(); // (23)
 
+const dummyData =[
+  {
+    id:1,
+    emotion:1,
+    content: "오늘의 일기 1번",
+    date: 1699432776920,
+  },
+  {
+    id:2,
+    emotion:2,
+    content: "오늘의 일기 2번",
+    date: 1699432776921,
+  },
+  {
+    id:3,
+    emotion:3,
+    content: "오늘의 일기 3번",
+    date: 1699432776922,
+  },
+  {
+    id:4,
+    emotion:4,
+    content: "오늘의 일기 4번",
+    date: 1699432776923,
+  },
+  {
+    id:5,
+    emotion:5,
+    content: "오늘의 일기 5번",
+    date: 1699432776924,
+  },
+]
 
 function App() {
 
-  const [data,dispatch] = useReducer(reducer,[])  // (1) reducer 함수 전달, 기본 state는 빈배열
+  const [data,dispatch] = useReducer(reducer,dummyData)  // (1) reducer 함수 전달, 기본 state는 빈배열
 
+  console.log(new Date().getTime());
   
   const dataId = useRef(0); // (15) 
   // CREATE
