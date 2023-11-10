a9c592acc4e1a259b88f09fd3ee8ba7442760b53
hwanghaein <hhi9037@naver.com>
2023-11-10 15:04:51 +0900

    감정 필터 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -5,6 +5,12 @@ const sortOptionList = [
   { value: "oldest", name: "오래된 순" },
 ];
 
+const filterOptionList = [
+  { value: "all", name: "전부 다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
+
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
@@ -19,21 +25,32 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
 
-  // (10) 필터에 따라 리스트 정렬 (최신순인지 오래된순인지 if문으로 분기를 달아서 정렬된 리스트를 반환하는 함수)
-  const getProcessedDiaryList = () => { 
+  const getProcessedDiaryList = () => {
 
-  const compare = (a,b)=> { // 비교해주는 함수
-    if(sortType ==='latest'){
-      return parseInt(b.date) - parseInt(a.date); // 문자열이 들어올수도 있어서 parseInt로 숫자로 바꿔줌
-    }else{
-      return parseInt(a.date) - parseInt(BeforeUnloadEvent.date);
-    }
-  }
-  const copyList = JSON.parse(JSON.stringify(diaryList));
-  const sortedList = copyList.sort(compare);
-  return sortedList;
-  }
+    const filterCallBack = (item) => {  
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(BeforeUnloadEvent.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+
+    const filteredList = filter === "all" ? copyList : copyList.filter((it)=>filterCallBack(it)); 
+
+    const sortedList = filteredList.sort(compare); 
+    return sortedList;
+  };
 
   return (
     <div>
@@ -42,8 +59,16 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content}
+          {it.emotion}
+        </div>
       ))}
     </div>
   );
